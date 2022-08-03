59d6fb0a7085a441db6a3c23f64a79448d3004c3
Ale <alearanibar@gmail.com>
2022-08-03 12:09:40 -0700

    adding a tweet and comment throught app complete ready to deploy

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
-import { Tweet, Comment } from "../typings";
+import React, { useEffect, useState } from "react";
+import { Tweet, Comment, CommentBody } from "../typings";
 import TimeAgo from "react-timeago";
 import {
   ChatAlt2Icon,
@@ -9,22 +9,56 @@ import {
   UploadIcon,
 } from "@heroicons/react/outline";
 import { fetchComments } from "../utils/fetchComments";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 
 interface Props {
   tweet: Tweet;
 }
 
 const Tweet = ({ tweet }: Props) => {
+  const [input, setInput] = useState<string>("");
   const [comments, setComments] = React.useState<Comment[]>([]);
+  const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false);
+
+  const { data: session } = useSession();
 
   const refreshComments = async () => {
     const comments: Comment[] = await fetchComments(tweet._id);
     setComments(comments);
   };
-
+  console.log("session", tweet);
   useEffect(() => {
     refreshComments();
   }, []);
+
+  const postComment = async () => {
+    const commentInfo: CommentBody = {
+      comment: input,
+      tweetId: tweet._id,
+      username: session?.user?.name || "Unknown user",
+      profileImg:
+        session?.user?.image ||
+        "https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png",
+    };
+    const results = await fetch(`/api/addComment`, {
+      body: JSON.stringify(commentInfo),
+      method: "POST",
+    });
+    const json = await results.json();
+    refreshComments();
+
+    toast("Tweet posted!", {
+      icon: "🚀",
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    postComment();
+    setInput("");
+    setCommentBoxVisible(false);
+  };
 
   return (
     <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
@@ -54,7 +88,10 @@ const Tweet = ({ tweet }: Props) => {
         </div>
       </div>
       <div className="mt-5 flex justify-between">
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+        <div
+          onClick={() => session && setCommentBoxVisible(!commentBoxVisible)}
+          className="flex cursor-pointer items-center space-x-3 text-gray-400"
+        >
           <ChatAlt2Icon className="h-5 w-5 " />
           <p>{comments.length}</p>
         </div>
@@ -68,8 +105,27 @@ const Tweet = ({ tweet }: Props) => {
           <UploadIcon className="h-5 w-5 " />
         </div>
       </div>
+
+      {commentBoxVisible && (
+        <form onSubmit={handleSubmit} className="mt-3 flex space-x-3">
+          <input
+            className="flex-1 rounded-lg bg-gray-100 p-2 putline-none"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Write a comment..."
+          />
+          <button
+            disabled={!input}
+            type="submit"
+            className="text-twitter disabled:text-gray-200"
+          >
+            Post
+          </button>
+        </form>
+      )}
       {comments?.length > 0 && (
-        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5/">
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t scrollbar-hide border-gray-100 p-5/">
           {comments.map((comment) => (
             <div key={comment._id} className=" relative flex space-x-2">
               <hr className="absolute left-5 top-10 h-8  border-x border-twitter/30" />
--- pages/api/addTweet.ts
+++ pages/api/addTweet.ts
@@ -3,16 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { TweetBody } from '../../typings'
 
 type Data = {
-  name: string
+  message: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
 
   const data: TweetBody = JSON.parse(req.body)
-
   const mutations = {
     mutations: [
       {
@@ -28,18 +27,17 @@ export default function handler(
     ]
   }
 
-  const apiEndpoint = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
-
-  const result = await fetch(apiEndpoint, {
+  const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
+const result = await fetch(apiEndpoint, {
     headers: {
       'content-type': 'application/json',
-      Authorization: `Bearer ${process.env.SANITY_API_KEY}`
+      Authorization: `Bearer ${process.env.SANITY_API_TOKEN}`
     },
     body: JSON.stringify(mutations),
     method: 'POST'
   })
+  const json = await result.json()
 
-  const json =await result.json()
-
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ message: 'Tweet added' })
 }
+ 
