ef665892f90f91d290401306e12ccf446b381af1
Ale <alearanibar@gmail.com>
2022-08-04 13:42:19 -0700

    debug 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "www.iconpacks.net",
