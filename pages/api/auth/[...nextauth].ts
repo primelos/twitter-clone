b070e622eb1f22ac8f06d8c778c9fe430fcf13e8
Ale <alearanibar@gmail.com>
2022-08-04 20:47:13 -0700

    debug 5

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -15,8 +15,8 @@ export default NextAuth({
     //   clientSecret: process.env.GITHUB_SECRET,
     // }),
     TwitterProvider({
-      clientId: `${process.env.TWITTER_CLIENT_ID}`,
-      clientSecret:`${ process.env.TWITTER_CLIENT_SECRET}`,
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: "2.0",
     }),
     // ...add more providers here
--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -3,7 +3,7 @@ import { Tweet } from "../typings"
 const val = process.env.NEXT_PUBLIC_BASE_URL
 
 export const fetchTweets = async () => {
-  console.log('val', val)
+  
   const res = await fetch(new URL(`${val}/api/getTweets`))
   const data = await res.json()
   const tweets: Tweet[] = data.tweets
